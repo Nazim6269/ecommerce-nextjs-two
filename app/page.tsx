cfa3b3f6ff0847d42cc26f437194c10f78361885
Nazim Uddin <nazimuddin102001@gmail.com>
2024-12-15 09:28:55 +0600

    e commerce template with nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,79 @@
-import Image from "next/image";
+import AccountOverview from "@/components/Account/AccountOverview";
+import Breadcrumbs from "@/components/breadcrumbs/Breadcrumbs";
+import Categories from "@/components/Categories/Categories";
+import CustomerSection from "@/components/Customer/CustomerSection";
+import FilterSidebar from "@/components/filteringBar/FilterSideBar";
+import Footer from "@/components/footer/Footer";
+import Hero from "@/components/heroSection/Hero";
+import Login from "@/components/login/Login";
+import Navbar from "@/components/navbar/Navbar";
+
+import OrderConfirmation from "@/components/orderConfirmation/OrderConfirmation";
+import OrderReview from "@/components/orderReview/OrderReview";
+import OrderSummary from "@/components/orderSummary/OrderSummary";
+import OrderTracking from "@/components/orderTracking/OrderTracking";
+import PaymentForm from "@/components/paymentForm/PaymentForm";
+import ProductDesc from "@/components/productDesc/ProductDesc";
+import ProductDetails from "@/components/productDetails/ProductDetails";
+import ProductList from "@/components/productList/ProductList";
+import ProductReview from "@/components/productReview/ProductReview";
+import RefuncOverview from "@/components/refundOverview/RefuncOverview";
+import RefundStatus from "@/components/refundStatus/RefundStatus";
+import Register from "@/components/register/Register";
+import RepairProduct from "@/components/repairProduct/RepairProduct";
+import ReviewHistory from "@/components/reviewHistory/ReviewHistory";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <>
+      <div className="">
+        {/* Navbar */}
+        <Navbar />
+        {/* Breadcrums */}
+        <Breadcrumbs />
+        {/* Hero */}
+        <Hero />
+        {/* Categories */}
+        <Categories />
+        {/* Login */}
+        <Login />
+        {/* Register */}
+        <Register />
+        {/* product list */}
+        <ProductList />
+        {/* Filter side bar */}
+        <FilterSidebar />
+        {/* Product details */}
+        <ProductDetails />
+        {/* Product description */}
+        <ProductDesc />
+        {/* Product Review */}
+        <ProductReview />
+        {/* Order Summary */}
+        <OrderSummary />
+        {/* Order Review */}
+        <OrderReview />
+        {/* Order confirmation */}
+        <OrderConfirmation />
+        {/* Review history */}
+        <ReviewHistory />
+        {/* payment form */}
+        <PaymentForm />
+        {/* Customer section */}
+        <CustomerSection />
+        {/* Account over view  */}
+        <AccountOverview />
+        {/* Order Tracking */}
+        <OrderTracking />
+        {/* Refund Over view */}
+        <RefuncOverview />
+        {/* Refund status */}
+        <RefundStatus />
+        {/* Repair product */}
+        <RepairProduct />
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
   );
 }
